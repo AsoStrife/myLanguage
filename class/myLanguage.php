48bfa333010a61cd6874a73cebf2112791f1166e
Andrea <info@webenterprises.it>
2013-11-02 17:32:40 +0100

    Version 1.1.3
    Comment revision

--- class/myLanguage.php
+++ class/myLanguage.php
@@ -1,10 +1,10 @@
 <?php 
 /**
- * @author 		Corrriga Andrea <andreacorriga@gmail.com>
- * @copyright 		2013 Corriga Andrea (http://webenterprises.it)
- * @license 		http://www.opensource.org/licenses/bsd-license.php New BSD Licence
- * @version 		1.1.0
- * @link 		http://webenterprises.it/
+ * @author Corrriga Andrea <andreacorriga@gmail.com>
+ * @copyright 2013 Corriga Andrea (http://webenterprises.it)
+ * @license http://www.opensource.org/licenses/bsd-license.php New BSD Licence
+ * @version 1.1.0
+ * @link http://webenterprises.it/
 **/
 class myLanguage {
 	
