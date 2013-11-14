8c80540d0001807042f330c741db62b92cb4e08e
Andrea <cloud_aso@hotmail.it>
2013-11-14 22:35:40 +0100

    Update myLanguage.php

--- class/myLanguage.php
+++ class/myLanguage.php
@@ -1,10 +1,10 @@
 <?php 
 /**
- * @author Corrriga Andrea <andreacorriga@gmail.com>
- * @copyright 2013 Corriga Andrea (http://webenterprises.it)
+ * @author Corrriga Andrea <me@andreacorriga.com>
+ * @copyright 2013 Corriga Andrea (http://andreacorriga.com)
  * @license http://www.opensource.org/licenses/bsd-license.php New BSD Licence
  * @version 1.1.0
- * @link http://webenterprises.it/
+ * @link http://andreacorriga.com
 **/
 class myLanguage {
 	
